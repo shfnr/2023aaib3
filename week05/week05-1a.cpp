2cf127cf55c531e0a6a989b7ce57ff4d5a99aeb2
shfnr <145088388+shfnr@users.noreply.github.com>
2024-10-09 11:53:27 +0800

    Add files via upload

---
+++ week05/week05-1a.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	string s;
+	cin >> s;
+	for(char c : s){
+		if(isupper(c)) c = tolower(c);
+		else if(islower(c)) c = toupper(c);
+		cout << c;
+	}
+	//cout << s;
+	cout << "\n";
+}
