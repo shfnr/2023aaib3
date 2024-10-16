9d8c69d493941c47d3ae4a808b030a7013fd85cc
shfnr <145088388+shfnr@users.noreply.github.com>
2024-10-16 11:59:28 +0800

    Add files via upload

---
+++ week06/week06-1.cpp
@@ -0,0 +1,19 @@
+/// week06-1.cpp
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    vector<int> a;
+    a.push_back(30);
+    a.push_back(20);
+    a.push_back(10);
+    for(int i=0; i<3; i++){
+        cout << a[i] << "\n";
+    }
+    sort(a.begin(), a.end());
+    for(int i=0; i<3; i++){
+        cout << a[i] << " ";
+    }
+}
