4700ad9ac1918d9aa5e79822a42e482d4b09ae1e
shfnr <145088388+shfnr@users.noreply.github.com>
2024-11-13 11:28:59 +0800

    Add files via upload

---
+++ week10/week10-6.cpp
@@ -0,0 +1,16 @@
+//week10-6
+class Solution {
+public:
+    int arraySign(vector<int>& nums) {
+        int total = 1;
+        for(int i=0;i<nums.size();i++){
+            //total *= nums[i];
+            if(nums[i]>0) total *= 1;
+            if(nums[i]<0) total *= -1;
+            if(nums[i]==0) total *= 0;
+        }
+        if(total>0) return 1;
+        if(total<0) return -1;
+        return 0;
+    }
+};
