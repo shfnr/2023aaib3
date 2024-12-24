fd72367f050b2c6d9b18436000eb5df061f68023
shfnr <145088388+shfnr@users.noreply.github.com>
2024-12-25 00:37:21 +0800

    Add files via upload

---
+++ week14/week14-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* ans = new ListNode();
+        ListNode* now = ans;
+        while( l1 !=nullptr){
+            now -> next = new ListNode( l1 -> val );
+            l1 = l1 -> next;
+        }
+        return ans -> next;
+    }
+};
